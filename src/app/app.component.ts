87fd0894caa66d4472f18422afb75e554d1a8e20
matik802 <mati88846@gmail.com>
2023-11-16 01:08:25 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Mateusz_Klusek_AIooK_Zadanie_2';
+}
